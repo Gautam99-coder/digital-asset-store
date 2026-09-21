b3e2633789b01b66ff92a2db66d2022f1de13092
Gautam99-coder <186465364+Gautam99-coder@users.noreply.github.com>
2026-09-21 13:44:31 +0530

    feat: set up authentication routes and middleware in the app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express=require("express")
 const cors=require("cors")
+const authRoutes=require("./routes/authRoutes")
+const cookieParser=require("cookie-parser")
 
 const app=express()
 
 app.use(express.json())
 app.use(cors())
+app.use(cookieParser());
+
+app.use("/api/auth/",authRoutes)
 
 app.get("/",(req,res)=>{
     res.send("Digital Asset Store Api is running...");
